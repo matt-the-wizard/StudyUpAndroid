0c0a2fd03880c5c9c976caebefc86524f228db40
Matthew Berger <matthewberger@finsync.com>
2015-11-22 20:33:45 -0500

    setup task for loading students

---
+++ app/src/main/java/studyup/projects/ggc/tasks/LoadStudentsAsyncTask.java
@@ -0,0 +1,15 @@
+package studyup.projects.ggc.tasks;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by matt-the-wizard on 11/22/15.
+ */
+public class LoadStudentsAsyncTask extends AsyncTask<Void, Void, String> {
+
+
+    @Override
+    protected String doInBackground(Void... params) {
+        return null;
+    }
+}
