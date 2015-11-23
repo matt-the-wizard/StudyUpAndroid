66112a8ed2d9fab884d6881e857e6d07e33c5363
Matthew Berger <matthewberger@finsync.com>
2015-11-22 19:56:45 -0500

    setup async task for posting student record to rails server, work in progress

---
+++ app/src/main/java/studyup/projects/ggc/tasks/CreateProfileAsyncTask.java
@@ -0,0 +1,56 @@
+package studyup.projects.ggc.tasks;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by matt-the-wizard on 11/22/15.
+ */
+public class CreateProfileAsyncTask extends AsyncTask<Void, Void, String> {
+    private String jsonURL;
+
+    public CreateProfileAsyncTask(String username, String password, String firstName, String lastName, String institution) {
+        this.jsonURL = "https://localhost:3000/api/students?username=" + username
+                + "&password=" + password
+                + "&first_name=" + firstName
+                + "&last_name=" + lastName
+                + "&institution=" + institution;
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+        HttpURLConnection urlConnection = null;
+        URL url = null;
+        OutputStream outStream = null;
+        String response = "";
+        try {
+            url = new URL(this.jsonURL);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("POST");
+            urlConnection.connect();
+            outStream = urlConnection.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream, "UTF-8"));
+            writer.write(this.jsonURL);
+            writer.flush();
+            writer.close();
+            outStream.close();
+            int responseCode = urlConnection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                String line;
+                BufferedReader br=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                while ((line = br.readLine()) != null)
+                    response += line;
+            }
+            else
+                response = "";
+        } catch (Exception e) {}
+        return response;
+    }
+}
