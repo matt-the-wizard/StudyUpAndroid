8f53df478bfca7e537c1f59288108fe5030493ef
Matthew Berger <matthewberger@finsync.com>
2015-11-30 16:58:48 -0500

    instantiated user input fields for accessing account info

--- app/src/main/java/studyup/projects/ggc/controllers/CreateProfileActivity.java
+++ app/src/main/java/studyup/projects/ggc/controllers/CreateProfileActivity.java
@@ -6,15 +6,35 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import studyup.projects.ggc.controllers.R;
+import studyup.projects.ggc.tasks.CreateProfileAsyncTask;
 
 public class CreateProfileActivity extends AppCompatActivity {
+    private Button submitAccountButton;
+    private EditText username, password, passwordDigest, firstName, lastName, institution;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+        this.submitAccountButton = (Button) findViewById(R.id.submit_account_button);
+        this.username = (EditText) findViewById(R.id.username_value);
+        this.password = (EditText) findViewById(R.id.password_value);
+        this.passwordDigest = (EditText) findViewById(R.id.password_digest_value);
+        this.firstName = (EditText) findViewById(R.id.first_name_value);
+        this.lastName = (EditText) findViewById(R.id.last_name_value);
+        this.institution = (EditText) findViewById(R.id.institution_value);
+
+        this.submitAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+                CreateProfileAsyncTask task = new CreateProfileAsyncTask();
+            }
+        });
     }
 
 }
